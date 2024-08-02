234a149e78fb99c596c712d04e60a9b4f76f2e52
thoffman13 <thoffman9@unl.edu>
2024-08-01 23:15:22 -0500

    Attempt to fix build errors

--- types/LeadingUser.ts
+++ types/LeadingUser.ts
@@ -1,6 +1,6 @@
 export interface LeadingUser {
-  userId: string;
-  userName: string;
-  totalPurchased: number;
-  totalSpent: number;
+  userId?: string;
+  userName?: string;
+  totalPurchased?: number;
+  totalSpent?: number;
 }
