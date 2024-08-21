bf38a35d4e66142b9a6ee2f911f35fe67b6466af
thoffman13 <thoffman9@unl.edu>
2024-08-20 21:38:14 -0500

    Implemented total gallons drank feature

--- app/actions/getItemCost.ts
+++ app/actions/getItemCost.ts
@@ -5,6 +5,7 @@ import { auth } from '@clerk/nextjs/server';
 async function getItemCost(): Promise<{
   totalPurchases?: number;
   totalCosts?: number;
+  totalVolume?: number;
   error?: string;
 }> {
   const { userId } = auth();
@@ -24,7 +25,25 @@ async function getItemCost(): Promise<{
     const costs = transactions.map((transaction) => transaction.price);
     const totalCosts = costs.reduce((acc, item) => (acc += item), 0);
 
-    return { totalPurchases, totalCosts };
+    const volume = transactions.map((transaction) => {
+      if(transaction.measurementUnit === 'oz') {
+        return (transaction.volume * transaction.quantity) / 128;
+      } else if (transaction.measurementUnit === 'ml') {
+        return (transaction.volume * transaction.quantity) / 3785.41178;
+      } else if (transaction.measurementUnit === 'L') {
+        return (transaction.volume * transaction.quantity) / 3.78541178;
+      } else {
+        return 0;
+      }
+    });
+
+    const totalVolume = volume.reduce((acc, item) => (acc += item), 0);
+    
+    
+    //oz -> gallon 1/128 ml -> gallon = 1/3785.41178 L -> gallon = 1 / 3.78541178
+    
+
+    return { totalPurchases, totalCosts, totalVolume };
   } catch (error) {
     return { error: 'Database error' };
   }
--- app/actions/getRunningTotal.ts
+++ app/actions/getRunningTotal.ts
@@ -5,6 +5,7 @@ import { auth } from '@clerk/nextjs/server';
 async function getRunningTotal(): Promise<{
   totalPurchases?: number;
   totalCosts?: number;
+  totalVolume?: number;
   error?: string;
 }> {
     try {
@@ -16,7 +17,21 @@ async function getRunningTotal(): Promise<{
     const costs = transactions.map((transaction) => transaction.price);
     const totalCosts = costs.reduce((acc, item) => (acc += item), 0);
 
-    return { totalPurchases, totalCosts };
+    const volume = transactions.map((transaction) => {
+      if(transaction.measurementUnit === 'oz') {
+        return (transaction.volume * transaction.quantity) / 128;
+      } else if (transaction.measurementUnit === 'ml') {
+        return (transaction.volume * transaction.quantity) / 3785.41178;
+      } else if (transaction.measurementUnit === 'L') {
+        return (transaction.volume * transaction.quantity) / 3.78541178;
+      } else {
+        return 0;
+      }
+    });
+
+    const totalVolume = volume.reduce((acc, item) => (acc += item), 0);
+
+    return { totalPurchases, totalCosts, totalVolume };
   } catch (error) {
     return { error: 'Database error' };
   }
