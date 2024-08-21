d8d629208cf8d6abb4b2b195ebb68d9504d49e05
thoffman13 <thoffman9@unl.edu>
2024-08-20 21:37:50 -0500

    Implemented volume / units feature

--- app/actions/addTransaction.ts
+++ app/actions/addTransaction.ts
@@ -7,6 +7,8 @@ interface TransactionData {
   text: string;
   quantity: number;
   price: number;
+  volume: number;
+  measurementUnit: string;
 }
 
 interface TransactionResult {
@@ -18,15 +20,19 @@ async function addTransaction(formData: FormData): Promise<TransactionResult> {
   const textValue = formData.get('text');
   const quantityValue = formData.get('quantity');
   const priceValue = formData.get('price');
+  const volumeValue = formData.get('volume');
+  const measurementUnitValue = formData.get('measurementUnit');
 
   // Check for input values
-  if (!textValue || textValue === '' || !quantityValue || !priceValue) {
+  if (!textValue || textValue === '' || !quantityValue || !priceValue || !volumeValue || !measurementUnitValue) {
     return { error: 'Required field is missing' };
   }
 
   const text: string = textValue.toString(); // Ensure text is a string
   const quantity: number = parseInt(quantityValue.toString()); //Parse quantity as integer
   const price: number = parseFloat(priceValue.toString()); // Parse price as float
+  const volume: number = parseFloat(volumeValue.toString()); // Parse volume as float
+  const measurementUnit: string = measurementUnitValue.toString(); 
 
   // Get logged in user
   const { userId } = auth();
@@ -42,6 +48,8 @@ async function addTransaction(formData: FormData): Promise<TransactionResult> {
         text,
         quantity,
         price,
+        volume,
+        measurementUnit,
         userId,
       },
     });
